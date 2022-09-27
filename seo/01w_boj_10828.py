4b49766be7a1a4ecd2d8d55dcc81f1b01f53eefc
LSJ <1007ckddjs12@gmail.com>
2022-09-27 17:54:46 +0900

    LSJ_0917

---
+++ seo/01w_boj_10828.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = []
+for i in range(n):
+    istr = sys.stdin.readline().split()
+
+    if istr[0] == "push":
+        arr.append(istr[1])
+    elif istr[0] == "pop":
+        if len(arr) == 0:
+            print(-1)
+        else:
+            print(arr.pop())
+    elif istr[0] == "size":
+        print(len(arr))
+    elif istr[0] == "empty":
+        if len(arr) == 0:
+            print(1)
+        else:
+            print(0)
+    elif istr[0] == "top":
+        if len(arr) == 0:
+            print(-1)
+        else:
+            print(arr[-1])
