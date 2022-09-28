92a2016d843fcab1439ceedbdba908851a6e66d4
LSJ <1007ckddjs12@gmail.com>
2022-09-28 16:51:10 +0900

    LSJ_0928

---
+++ seo/01w_boj_1874.py
@@ -0,0 +1,26 @@
+import sys
+
+base = []  # 오름차순 수열
+ex_arr = [] # 예제 수열
+arr = []    # 풀이에 사용할 리스트
+print_arr = []  # +- 출력 내용을 저장한 리스트
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    base[i] = i
+
+for i in range(n):
+    num = int(sys.stdin.readline())
+    ex_arr[i] = num
+
+
+
+
+
+# 마지막 출력
+if:
+    for i in range(len(print_arr)):
+        print(print_arr[i])
+else:
+    print("NO")
