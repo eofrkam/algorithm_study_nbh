11fa632e1d367b9d243870834f3b0f7ddc24fdf7
LeeSJ0516 <1007ckddjs12@gmail.com>
2022-09-29 21:11:10 +0900

    LSJ_0929

--- seo/01w_boj_1874.py
+++ seo/01w_boj_1874.py
@@ -1,81 +1,44 @@
 import sys
 
+arr9 = []       # TEST1
+arr2 = []       # TEST2
+
 base = []
-ex_arr = [] # 예제 수열
-arr = []    # 풀이에 사용할 리스트
+arr = []        # 풀이에 사용할 리스트
+ex_arr = []     # 예제 수열
 print_arr = []  # +- 출력 내용을 저장한 리스트
 
-cnt = 1
-
 n = int(sys.stdin.readline())
-
-for i in range(1, n+1, 1):
-    base.append(i)
-
+for i in range(n):
     num = int(sys.stdin.readline())
     ex_arr.append(num)
 
-arr2 = []
+i = 0
+j = 0
+while i < n:
+    base.append(i+1)
+    print("+")
+    if base[i] == ex_arr[j]:
+        arr.append(base.pop())
+        j += 1
+        print("-")
+        continue
+    i += 1
 
-arr.append(1)
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr.append(2)
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr.append(3)
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr.append(4)
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr2.append(arr.pop())
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr2.append(arr.pop())
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr.append(5)
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr.append(6)
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr2.append(arr.pop())
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr.append(7)
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr.append(8)
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr2.append(arr.pop())
-print("arr = ", arr)
-print("arr2 = " ,arr2)
-arr2.append(arr.pop())
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr2.append(arr.pop())
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr2.append(arr.pop())
-print("arr = ", arr)
-print("arr2 = ", arr2)
-arr2.append(arr.pop())
-print("arr = ", arr)
-print("arr2 = ", arr2)
+print(base)
+print(arr)
 
-
-
-# 검산 출력
-for i in range(n):
-    print("ex_arr = <", ex_arr[i], ">", end=" | ")
-    print("base = [", base[i], "]")
-
-# # 마지막 출력
-# if:
-#     for i in range(len(print_arr)):
-#         print(print_arr[i])
-# else:
-#     print("NO")
+# i = 0
+# j = 0
+# while 1:
+#     if j == n:
+#         break
+#     if i < ex_arr[j]:
+#         i += 1
+#     elif i == ex_arr[j]:
+#         arr.append(i)
+#         j += 1
+#     elif i > ex_arr[j]:
+#         i -= 1
+#
+# print(arr)
