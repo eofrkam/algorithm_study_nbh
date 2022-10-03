09770b38cf2c93ef49ea403f554dce51e3300dc3
LeeSJ0516 <1007ckddjs12@gmail.com>
2022-10-03 22:03:39 +0900

    LSJ_1003(not clear)

---
+++ seo/01w_boj_17298.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+base = list(map(int, sys.stdin.readline().split()))
+
+nge = []
+arr = []
+
+
+
+
+
+
+nge.append(-1)
+for i in range(len(nge)):
+    print(nge[i], end=' ')
