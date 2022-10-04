917c9067dd8b3790dede3ec265c7499f968e607a
LeeSJ0516 <1007ckddjs12@gmail.com>
2022-10-04 19:44:32 +0900

    LSJ_1003(not clear)

--- seo/01w_boj_17298.py
+++ seo/01w_boj_17298.py
@@ -1,16 +1,23 @@
 import sys
 
-n = int(sys.stdin.readline())
-base = list(map(int, sys.stdin.readline().split()))
-
 nge = []
 arr = []
+base = [0]
 
+n = int(sys.stdin.readline())
+base.extend((map(int, sys.stdin.readline().split())))
 
-
-
-
-
+arr.append(base.pop())
 nge.append(-1)
-for i in range(len(nge)):
+
+while base[-1] != 0:
+    if base[-1] > arr[-1] and base[-1] > nge[-1]:
+        nge.append(-1)
+    elif base[-1] > arr[-1] and base[-1] < nge[-1]:
+        nge.append(nge[-1])
+    elif base[-1] < arr[-1]:
+        nge.append(arr[-1])
+    arr.append(base.pop())
+
+for i in reversed(range(len(nge))):
     print(nge[i], end=' ')
