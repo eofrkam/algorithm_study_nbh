1b3f58613fcd25ff19dbce7902a4560adee1d628
LSJ <1007ckddjs12@gmail.com>
2022-10-12 16:29:15 +0900

    LSJ_1012

--- seo/APw/03w_boj_1654.py
+++ seo/APw/03w_boj_1654.py
@@ -15,17 +15,14 @@ k_min = k_list[0] // n
 k_max = k_list[-1]
 k_mid = (k_min + k_max) // 2
 
-flag = 2
 while k_min + 1 < k_max:
     k_len = 0
     k_mid = (k_min + k_max) // 2
     for i in range(len(k_list)):
         k_len += k_list[i] // k_mid
     if n <= k_len:
-        flag = 1
         k_min = k_mid
     else:
-        flag = 0
         k_max = k_mid
 
 print(k_min)
