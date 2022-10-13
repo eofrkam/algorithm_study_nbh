9dab59b9d05a5886fafbdab4ff7b92d5c5acbd60
LeeSJ0516 <1007ckddjs12@gmail.com>
2022-10-13 17:50:57 +0900

    LSJ_221013

--- seo/02w/02w_boj_1966.py
+++ seo/02w/02w_boj_1966.py
@@ -3,26 +3,26 @@ import sys
 
 t = int(sys.stdin.readline())
 for i in range(t):
-    arr = deque()
-    temp = []
-
     n, m = map(int, sys.stdin.readline().split())
     imp = deque(map(int, sys.stdin.readline().split()))
 
-    for j in range(n):
+    arr = deque()       # queue
+    temp = []           # queue로 사용하기 위한 임시 list
+
+    for j in range(n):  # 중요도의 index
         arr.append(j)
 
-    idx = arr[m]
+    m_val = arr[m]      # m value
     cnt = 1
     while len(arr):
-        if imp[0] == max(imp):
-            res = arr.popleft()
-            tmp = imp.popleft()
-            if idx == res:
+        if imp[0] == max(imp):      # 중요도가 제일 높을때
+            p_val = arr.popleft()
+            imp.popleft()
+            if m_val == p_val:      # m번째 value와 pop value가 같을때
                 print(cnt)
                 break
             cnt += 1
-        else:
+        else:                       # temp list를 통해 queue 구현
             temp.append(arr.popleft())
             arr.append(temp.pop())
             temp.append(imp.popleft())
