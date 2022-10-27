f10985baf8e3ef64c718e9a21dcea1d760cf5a83
LeeSJ0516 <1007ckddjs12@gmail.com>
2022-10-27 16:25:55 +0900

    LSJ_22.10.27

--- seo/APw/04w_boj_2580.py
+++ seo/APw/04w_boj_2580.py
@@ -2,23 +2,25 @@ import sys
 
 
 def valid(row, col, board, value):
-    flag = 0
     x = (col // 3) * 3
     y = (row // 3) * 3
     for i in range(0, 9):
         if board[row][i] == value:
-            flag = 1
+            return 1
         if board[i][col] == value:
-            flag = 1
+            return 1
     for j in range(y, y + 3):
         for k in range(x, x + 3):
             if board[j][k] == value:
-                flag = 1
-    return flag
+                return 1
+    return 0
 
 
 def sudoku(row, col, board, zeros):
     if len(zeros) == 0 and row == 8 and col == 9:
+        for m in range(9):
+            print(*input_board[m])
+        exit()
         return
     if col == 9:
         row += 1
@@ -27,9 +29,13 @@ def sudoku(row, col, board, zeros):
         zeros.append([row, col])
         for val in range(1, 10):
             if valid(row, col, board, val) == 0:
+                if len(zeros) != 0:
+                    zeros.pop()
                 board[row][col] = val
-                zeros.pop()
-    sudoku(row, col + 1, board, zeros)
+                sudoku(row, col + 1, board, zeros)
+                board[row][col] = 0
+    else:
+        sudoku(row, col + 1, board, zeros)
 
 
 zrs = []
@@ -38,6 +44,3 @@ for n in range(9):
     input_board.append(list(map(int, sys.stdin.readline().split())))
 
 sudoku(0, 0, input_board, zrs)
-
-for m in range(9):
-    print(*input_board[m])
