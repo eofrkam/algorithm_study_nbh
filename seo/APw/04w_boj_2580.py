e410da075e1c8b065fbf1f02ed578a7b3b3426d7
LeeSJ0516 <1007ckddjs12@gmail.com>
2022-10-27 14:07:24 +0900

    LSJ_22.try

--- seo/APw/04w_boj_2580.py
+++ seo/APw/04w_boj_2580.py
@@ -1,49 +1,43 @@
 import sys
 
 
-def sector(row, col, board, value):
-    flag = 1
+def valid(row, col, board, value):
+    flag = 0
     x = (col // 3) * 3
     y = (row // 3) * 3
-    for z in range(y, y+3):
-        for v in range(x, x+3):
-            if board[z][v] == value:
-                flag = 0
+    for i in range(0, 9):
+        if board[row][i] == value:
+            flag = 1
+        if board[i][col] == value:
+            flag = 1
+    for j in range(y, y + 3):
+        for k in range(x, x + 3):
+            if board[j][k] == value:
+                flag = 1
     return flag
 
 
-def valid(row, col, board, value):
-    flag = 1
-    for j in range(0, 9):
-        if board[row][j] == value:
-            flag = 0
-        if board[j][col] == value:
-            flag = 0
-    if sector(row, col, board, value) == 0:
-        flag = 0
-    return flag
-
-
-# 실패 했을때 0으로 바꿔주는 부분 필요
-def sudoku(row, col, board):
-    if row == 8 and col == 9:
+def sudoku(row, col, board, zeros):
+    if len(zeros) == 0 and row == 8 and col == 9:
         return
     if col == 9:
         row += 1
         col = 0
-    else:
-        if board[row][col] == 0:
-            for i in range(1, 10):
-                if valid(row, col, board, i) == 1:
-                    board[row][col] = i
-    sudoku(row, col + 1, board)
+    if board[row][col] == 0:
+        zeros.append([row, col])
+        for val in range(1, 10):
+            if valid(row, col, board, val) == 0:
+                board[row][col] = val
+                zeros.pop()
+    sudoku(row, col + 1, board, zeros)
 
 
+zrs = []
 input_board = []
 for n in range(9):
     input_board.append(list(map(int, sys.stdin.readline().split())))
 
-sudoku(0, 0, input_board)
+sudoku(0, 0, input_board, zrs)
 
 for m in range(9):
     print(*input_board[m])
