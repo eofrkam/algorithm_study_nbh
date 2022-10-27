4cee5df0a8746a5aa630ee188ca3faac6df61028
LeeSJ0516 <1007ckddjs12@gmail.com>
2022-10-27 15:53:19 +0900

    LSJ_22.try

--- seo/04w/04w_boj_14888.py
+++ seo/04w/04w_boj_14888.py
@@ -1,48 +1,39 @@
 import sys
 
 
-def cal(st, en, a, op, val):
+def rec(st, en, op, res, a):
     if st == en:
-        print(*val)
+        print(*res)
         return
-    for j in a:
-        if used[j] == j:
+    for i in op:
+        if used[i] == 1:
             continue
-        used[j] = j
-        val[st] = j
-        cal(st + 1, en, a, op, val)
-        used[j] = 0
+        used[i] = 1
+        res[st] = i
+        rec(st + 1, en, op, res, a)
+        used[i] = 0
 
 
 N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
 ops = list(map(int, sys.stdin.readline().split()))
 
-res = [0] * N
-used = [0] * (max(A)+1)
-operator = [0] * (N-1)
-for i in range(sum(ops)):
+operator = []
+for n in range(N-1):
     if ops[0] > 0:         # 덧셈 = 1
-        operator[i] = 1
+        operator.append(1)
         ops[0] -= 1
     elif ops[1] > 0:       # 뺄셈 = 2
-        operator[i] = 2
+        operator.append(2)
         ops[1] -= 1
     elif ops[2] > 0:       # 곱셈 = 3
-        operator[i] = 3
+        operator.append(3)
         ops[2] -= 1
     elif ops[3] > 0:       # 나눗셈 = 4
-        operator[i] = 4
+        operator.append(4)
         ops[3] -= 1
 
-cal(0, N, A, operator, res)
-print(operator)
+comb = [0] * len(operator)
+used = [0] * 5
 
-# if operator[i] == 1:
-#     res = x + y
-# elif operator[i] == 2:
-#     res = x - y
-# elif operator[i] == 3:
-#     res = x * y
-# elif operator[i] == 4:
-#     res = x // y
+rec(0, len(operator), operator, comb, A)
